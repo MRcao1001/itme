209465d761fd4626c2c044ce9b2901c205082d62
MRcao1001 <cy15056366084@outlook.com>
2018-03-31 18:23:29 +0800

    测试

--- interfaceUI.c
+++ interfaceUI.c
@@ -4,52 +4,83 @@
 
 void mainUI()
 {
-    printf("\t1. 管理员登录\n");
     printf("\n");
-    printf("\t2. 用户登录\n");
     printf("\n");
-    printf("\t0. 退出\n");
+    printf("\t\t欢迎使用彩票管理系统\n\n");
     printf("\n");
-    printf("\t请选择：");
+    printf("\t\t 1. 管理员用户登录\n");
+    printf("\n");
+    printf("\t\t　 2. 用户登录\n");
+    printf("\n");
+    printf("\t\t　　 0. 退出\n");
+    printf("\n");
 }
 
 void adminloginUI()
 {
-    printf("\t1. 管理员注册\n");
-    printf("\t2. 管理员登录\n");
-	printf("\t请选择：");
+    printf("\n");
+    printf("\n");
+    printf("\t\t欢迎使用彩票管理系统\n\n");
+    printf("\n");
+    printf("\t\t　1. 申请管理员\n");
+    printf("\n");
+    printf("\t\t  2. 已经管理员\n");
+	printf("\n");
     return;
 }
 
 void adminUI()
 {
-    printf("\t1. 发布彩票\n");
-    printf("\t2. 删除彩票\n");
-    printf("\t3. 查询彩票信息\n");
-    printf("\t4. 显示所有彩民\n");
-	printf("\t5. 开奖\n");
-    printf("\t0. 退出\n");
-    printf("\t请选择: ");
+    printf("\n");
+    printf("\n");
+    printf("\t\t     彩票管理系统\n\n");
+    printf("\n");
+    printf("\t\t     1. 发布彩票\n");
+    printf("\n");
+    printf("\t\t     2. 删除彩票\n");
+    printf("\n");
+    printf("\t\t   3. 查询彩票信息\n");
+    printf("\n");
+    printf("\t\t    4. 显示所有彩民\n");
+    printf("\n");
+	printf("\t\t      5. 开奖\n");
+    printf("\n");
+    printf("\t\t      0. 退出\n");
+    printf("\n");
     return;
 }
 
 void userloginUI()
 {
-    printf("\t1. 用户注册\n");
-    printf("\t2. 用户登录\n");
-	printf("\t请选择：");
+    printf("\n");
+    printf("\n");
+    printf("\t\t 欢迎使用彩票管理系统\n\n");
+    printf("\n");
+    printf("\t\t    1. 用户注册\n");
+    printf("\n");
+    printf("\t\t    2. 用户登录\n");
+    printf("\n");
     return;
 }
 
 void userUI()
 {
-    printf("\t1. 个人资料\n");
-    printf("\t2. 购买彩票\n");
-    printf("\t3. 购彩记录\n");
-    printf("\t4. 余额充值\n");
-    printf("\t5. 修改密码\n");
-    printf("\t0. 退出\n");
-    printf("\t请选择: ");
+    printf("\n");
+    printf("\n");
+    printf("\t\t 欢迎使用彩票管理系统\n\n");
+    printf("\n");
+    printf("\t\t    1. 个人资料\n");
+    printf("\n");
+    printf("\t\t    2. 购买彩票\n");
+    printf("\n");
+    printf("\t\t    3. 购彩记录\n");
+    printf("\n");
+    printf("\t\t    4. 余额充值\n");
+    printf("\n");
+    printf("\t\t    5. 修改密码\n");
+    printf("\n");
+    printf("\t\t    0. 退出\n");
+    printf("\n");
     return;
 }
 
